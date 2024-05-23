386dc4bbb1ae390d1e6ef614ce471568fc9e3f6a
Pridemore <pridemoreviriri@gmail.com>
2024-05-23 04:40:34 +0200

    initial local commit

---
+++ backend-api-service/src/main/java/com/smatech/backendapiservice/BackendApiServiceApplication.java
@@ -0,0 +1,13 @@
+package com.smatech.backendapiservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendApiServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackendApiServiceApplication.class, args);
+    }
+
+}
---
+++ backend-api-service/src/test/java/com/smatech/backendapiservice/BackendApiServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smatech.backendapiservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApiServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
